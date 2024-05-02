c8e9838a69561867d12dd203ea1a7377930c4126
Maxime Portaz <maximep@graphcore.ai>
2024-05-02 21:45:29 +0200

    update

--- vllm.py
+++ vllm.py
@@ -9,10 +9,9 @@ login(token='')
 
 # Sample prompts.
 prompts = [
-     "Hello, my name is",
-     "The president of the United States is",
-     "The capital of France is",
-     "The future of AI is",
+     'Provide step-by-step instructions on how to make a safe and effective homemade all-purpose cleaner from common household ingredients. The guide should include measurements, tips for storing the cleaner, and additional variations or scents that can be added. Additionally, the guide should be written in clear and concise language, with helpful visuals or photographs to aid in the process.',
+    'Write a personal essay discussing how embracing vulnerability and authenticity has affected your life. Use specific examples from your own experiences to support your arguments.',
+    'Did Karl Marx theories on centralizing credit have anything to do with our current central banking system?'
 ]
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=200)
